c86d93847324a8c91c5812e5401264a2c9f1ea62
Kiruthik Nandhan Murthi <38995539+mNandhu@users.noreply.github.com>
2025-03-08 20:42:49 +0530

    fix: Incorrect import and Refactor domain configurations into config.py and update imports across modules

--- src/data_collection/__init__.py
+++ src/data_collection/__init__.py
@@ -1,4 +1,3 @@
 from .wiki_data_service import get_data_from_wiki, get_and_save_from_wiki
-from .wikidata.queries import DOMAIN_CONFIGS
 
-__all__ = ["get_data_from_wiki", "get_and_save_from_wiki", "DOMAIN_CONFIGS"]
+__all__ = ["get_data_from_wiki", "get_and_save_from_wiki"]
